2901b0a17204aad5d7fad03431a6d6a3f851b770
Smokyduck <siver93@gmail.com>
2018-11-02 04:13:51 -0400

    invite-accepter added

---
+++ linkedin/invite-accepter.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         invite-accepter
+// @version      0.1
+// @updateURL    https://raw.githubusercontent.com/Smokyduck/tampermonkey-scripts/master/linkedin/invite-accepter.js
+// @downloadURL  https://raw.githubusercontent.com/Smokyduck/tampermonkey-scripts/master/linkedin/invite-accepter.js
+// @author       Smokyduck
+// @match        https://www.linkedin.com/search/results/*
+// ==/UserScript==
+
+function acceptInvite(){
+	if (document.body.querySelector(".button-primary-large.ml1")) {
+		if (document.body.querySelector(".button-primary-large.ml1").innerText.match(/Send/)) {
+	        document.body.querySelector(".button-primary-large.ml1").click();
+        }
+    }
+};
+
+document.body.addEventListener("keydown",
+                               q=>{if(event.key == 'Tab')
+                                       {acceptInvite();}
+                               },
+                               false);
