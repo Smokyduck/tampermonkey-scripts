bb151717d02a29ec7810695df57264437ae4f848
Smokyduck <siver93@gmail.com>
2018-11-30 04:06:33 -0500

    widering an area of the script

--- linkedin/invite-accepter.js
+++ linkedin/invite-accepter.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         invite-accepter
-// @version      0.1
+// @version      0.2
 // @updateURL    https://raw.githubusercontent.com/Smokyduck/tampermonkey-scripts/master/linkedin/invite-accepter.js
 // @downloadURL  https://raw.githubusercontent.com/Smokyduck/tampermonkey-scripts/master/linkedin/invite-accepter.js
 // @author       Smokyduck
-// @match        https://www.linkedin.com/search/results/*
+// @match        https://www.linkedin.com/*
 // ==/UserScript==
 
 function acceptInvite(){
